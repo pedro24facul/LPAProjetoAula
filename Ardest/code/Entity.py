d9ecaf457c6eafadf5bd09bf5e5c90ff3519c203
vidadeenderman@gmail.com <vidadeenderman@gmail.com>
2024-03-06 19:18:32 -0300

    primeiro commit do projeto lpa?

---
+++ Ardest/code/Entity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.name = None
+        self.surf = None
+        self.rect = None
+
+    def name(self, ):
+        pass
+
+    def rect(self, ):
+        pass
+
+    def move(self, ):
+        pass
+
+    def Operation2(self, ):
+        pass
+
